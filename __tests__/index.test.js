a4600708cb0a56655127c3b5d232858423d7e242
Joseph <z@charmed.tech>
2019-03-21 20:54:25 -0700

    Dummy test files

---
+++ __tests__/index.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const index = require('../index.js');
+
+describe('it should be good', () => {
+  index;
+  expect(true).toBeTruthy();
+});
---
+++ __tests__/web-formatter.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const webFormatter = require('../web-formatter.js');
+
+describe('it should be good', () => {
+  webFormatter;
+  expect(true).toBeTruthy();
+});
